c7e761daacceeba801db243188dafb4bfc872376
HamHibCoders1 <hum.hib.com@gmail.com>
2022-09-16 03:57:59 -0700

    Counter App

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native'
+import {React, useState,useEffect} from 'react'
+import { Button } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [state, setstate] = useState(0);
+  useEffect(() => {
+    document.title = 'Counter App';
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+<Text style={styles.textf}>Counter App</Text>
+  
+<Text  style={styles.textf}>{state}</Text>
+<View style={styles.Button}>
+<Button title="increment" onPress={()=>{setstate(state+1)}}/>
+<Button title="Decrement" color="#c23616" onPress={()=>{setstate(state-1)}}/>
+</View>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default App
+
+
+const styles =StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#f5f6fa'
   },
-});
+  Button:{
+    marginTop:20,
+
+  },
+textf:{
+  fontSize:40,
+  marginBottom:20,
+  color:'#273c75'
+}
+})
